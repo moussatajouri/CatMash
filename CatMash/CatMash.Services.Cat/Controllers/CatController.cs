126e75ec2b232c119c29c14743022e8c3dc0cbf1
PC-Moussa\Moussa <moussa.taj@gmail.com>
2019-01-08 14:16:27 +0100

    Init Solution and Cat service with a mock resources

---
+++ CatMash/CatMash.Services.Cat/Controllers/CatController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatMash.Services.Cat.Messages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatMash.Services.Cat.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("scores")]
+        public IActionResult GetScores()
+        {
+            var catscoresResponses = new GetCatScoresResponse
+            {
+                CatScores = new List<CatScore>()
+                {
+                    new CatScore{ CatId="1", Score=10 },
+                    new CatScore{ CatId="2", Score=3 },
+                    new CatScore{ CatId="3" }
+                }
+            };
+            return Ok(catscoresResponses);
+        }
+
+        [HttpPut]
+        [Route("vote/{catId}")]
+        public void Vote(string catId)
+        {
+        }
+    }
+}
---
+++ CatMash/CatMash.Services.Cat/Messages/CatScore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatMash.Services.Cat.Messages
+{
+    public class CatScore
+    {
+        public string CatId { get; set; }
+
+        public int Score { get; set; }
+    }
+}
