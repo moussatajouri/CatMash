efbf6978065c679614d4a8532348d38e5e06a3e2
PC-Moussa\Moussa <moussa.taj@gmail.com>
2019-01-09 02:37:13 +0100

    feat(data) add DAL and Unit Tests

---
+++ CatMash/CatMash.Services.Cat/DataAccess/ICatRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatMash.Services.Cat.DataAccess
+{
+    public interface ICatRepository
+    {
+        TCat GetCatById(int id);
+
+        IEnumerable<TCat> GetAllCat();
+
+        int AddVote(TVote vote);
+    }
+}
